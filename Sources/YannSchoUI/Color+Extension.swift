b78cfa56032874b011ff8f0575be8653a754455a
yannscho <yann.scholtes@me.com>
2026-05-12 16:55:27 +0200

    feature: color extension for hex

---
+++ Sources/YannSchoUI/Color+Extension.swift
@@ -0,0 +1,55 @@
+//  Color+Extension.swift
+//  YannSchoKit
+//  Created by Yann Scholtes on 12/05/2026.
+import Foundation
+import SwiftUI
+
+public extension Color {
+    init(hex: String) {
+        let trimmed = hex
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .replacingOccurrences(of: "#", with: "")
+
+        var value: UInt64 = 0
+        Scanner(string: trimmed).scanHexInt64(&value)
+
+        let r, g, b, a: Double
+        switch trimmed.count {
+        case 6:
+            r = Double((value & 0xFF0000) >> 16) / 255
+            g = Double((value & 0x00FF00) >> 8) / 255
+            b = Double(value & 0x0000FF) / 255
+            a = 1
+        case 8:
+            r = Double((value & 0xFF000000) >> 24) / 255
+            g = Double((value & 0x00FF0000) >> 16) / 255
+            b = Double((value & 0x0000FF00) >> 8) / 255
+            a = Double(value & 0x000000FF) / 255
+        default:
+            r = 0; g = 0; b = 0; a = 1
+        }
+
+        self.init(.sRGB, red: r, green: g, blue: b, opacity: a)
+    }
+
+    var hex: String {
+        #if canImport(UIKit)
+        let resolved = UIColor(self).cgColor
+        #else
+        let resolved = NSColor(self).cgColor
+        #endif
+
+        guard let components = resolved.components, components.count >= 3 else {
+            return "#000000"
+        }
+
+        let r = Int((components[0] * 255).rounded())
+        let g = Int((components[1] * 255).rounded())
+        let b = Int((components[2] * 255).rounded())
+        let a = components.count >= 4 ? Int((components[3] * 255).rounded()) : 255
+
+        return a == 255
+            ? String(format: "#%02X%02X%02X", r, g, b)
+            : String(format: "#%02X%02X%02X%02X", r, g, b, a)
+    }
+}
